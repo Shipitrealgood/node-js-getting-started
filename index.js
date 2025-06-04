a918f3df3bea4080719ed44d188ac85589fb8bec
Shipitrealgood <bryce.delrio@gmail.com>
2025-06-03 23:18:06 -0500

    Update index.js
    Signed-off-by: Shipitrealgood <bryce.delrio@gmail.com>

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const app = express()
 // --- Health probe ---
 app.get('/health', (req, res) => res.send('OK'));
 
+// --- parse JSON bodies ---
+app.use(express.json());
+
+// --- Zoom webhook endpoint ---
+app.post('/webhook', (req, res) => {
+  console.log('Webhook hit:', JSON.stringify(req.body));
+  res.sendStatus(200);          // quick “OK” reply
+});
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
